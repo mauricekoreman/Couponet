497de7e57cec2538b9670beae40ddc76e04624e6
mauricekoreman <mg.koreman99@gmail.com>
2022-10-27 00:11:06 +0200

    added firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,26 +5,16 @@ import { initializeAuth } from "firebase/auth";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID,
-  FIREBASE_MEASUREMENT_ID,
-} from "@env";
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDu7hj_qfoP4HHrkHw0EaT1Hm96l4bE6fo",
+  authDomain: "couponet-c8c94.firebaseapp.com",
+  projectId: "couponet-c8c94",
+  storageBucket: "couponet-c8c94.appspot.com",
+  messagingSenderId: "940705987429",
+  appId: "1:940705987429:web:3e76eff67b23b496ba7f8c",
+  measurementId: "G-S254V2136Q",
 };
 
 // Initialize Firebase
